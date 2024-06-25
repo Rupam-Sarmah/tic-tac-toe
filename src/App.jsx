083f928a6a82da8fd47ae9412bc703ead7ec925e
Rupam-Sarmah <r.sarmah1202@gmail.com>
2024-06-25 09:36:43 +0530

    lesson-74 creating first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "./components/player"
 
 function App() { 
 
@@ -5,16 +6,27 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
+          {/* <li>
+            <span className="player">
+              <span className="player-name">Player1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button> */}
+            {/* This is repeating-1 */}
+          {/* </li>
           <li>
-            <span className="player-name">Player1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player2</span>
-            <span className="player-symbol">0</span>
-         </li>
+            <span className="player">
+              <span className="player-name">Player2</span>
+              <span className="player-symbol">0</span>
+            </span>
+            <button>Edit</button> */}
+            {/* This is repeating-2 */}
+            {/* for these two we can create a component */}
+         {/* </li> */}
+         <Player name="Player1" symbol="X" />
+         <Player name="Player2" symbol="0" />
         </ol>
-
+        Game Board
       </div>
     </main>
     
