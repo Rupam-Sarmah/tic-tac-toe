83606d74e0a48128b1a3fb520f60a207552a9ab5
Rupam-Sarmah <r.sarmah1202@gmail.com>
2024-07-01 09:33:13 +0530

    start working with state

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 export default function Player({name, symbol}){
+    const [isEditing, setIsEditing] =useState(false);
+
+    function handleEditClick(){
+        setIsEditing(true);
+    }
+    let playername= <span className="player-name">{name}</span>;
+    if(isEditing){
+        playername=<input type="text" required />
+    }
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onclick={handleEditClick}>Edit</button>
+            {/* edit button + save button . so need to use useSatate here */}
         </li>
     );
 }
